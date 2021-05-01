a05cd03324299526339789885933a7ef9c50bda3
JeeanLiima97 <jean.jr.souza@hotmail.com>
2021-05-01 11:14:14 -0300

    2 files modified

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const disciplinas = ["Compiladores", "Poo", "Banco de dados"];
+const aulas = [];
 const professores = [];
 const appDiv = document.getElementById("disciplinas");
 const form = document.getElementById("formId");
 const saveButton = document.getElementById("saveId");
+const saveButtonAula = document.getElementById("saveIdAula");
 const cardFormId = document.getElementById("cardFormId");
 const cardFormIdAula = document.getElementById("cardFormIdAula");
 
@@ -32,6 +34,17 @@ saveButton.onclick = () => {
   professores.push(value);
   showTable();
 };
+saveButtonAula.onclick = () => {
+  const value = {
+    id: ++idCount
+  };
+  for (var i = 0; i < form.elements.length; i++) {
+    const item = form.elements.item(i);
+    value[item.name] = item.value;
+  }
+  aulas.push(value);
+  showTableAula();
+};
 
 function showTable() {
   const table = document.getElementById("tableId");
@@ -57,6 +70,28 @@ function showTable() {
     <td>${it.femail}</td>
     <td>${it.fendereco}</td>
     <td>${it.ftelefone}</td>
+    </tr>
+    `;
+    })
+    .join();
+}
+
+function showTableAula() {
+  const table = document.getElementById("tableIdAula");
+
+  table.innerHTML = `
+    <tr>
+      <th>Professor</th>
+    <th>Disciplina</th>
+    <th>Alunos</th>
+  </tr>`;
+  table.innerHTML += aulas
+    .map(it => {
+      return `
+      <tr>
+    <td>${it.fprofessor}</td>
+    <td>${it.fdisciplina}</td>
+    <td>${it.falunos}</td>
     </tr>
     `;
     })
